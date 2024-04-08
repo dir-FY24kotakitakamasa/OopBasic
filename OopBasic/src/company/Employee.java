21fedaecf5a20b60bc0360dce861adc7df3594e0
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-08 15:28:16 +0900

    インターフェース、抽象化クラス 要復習ですね。

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -2,9 +2,9 @@ package company;
 
 import java.security.PublicKey;
 
-public class Employee {
+public abstract class Employee implements Workable {
     protected final String name;
-    private final Department department;
+    protected final Department department;
     private final String position;
     private final int employeeId;
     
@@ -23,9 +23,12 @@ public class Employee {
         report(1);
     }
     
-    public void joinMeeting() {
-        department.meeting();
-        System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" + name);
+    public abstract void joinMeeting();
+    
+    @Override
+    public void work() {
+        System.out.println("正社員として働きます。名前：" + name + slogan);
+        
     }
 
 }
---
+++ OopBasic/src/company/Sales.java
@@ -0,0 +1,15 @@
+package company;
+
+import javax.management.remote.SubjectDelegationPermission;
+
+public class Sales extends Employee{
+    public Sales(String name, Department department, String position, int employeeId) {
+        super(name, department, position, employeeId);
+    }
+        
+    @Override
+    public void joinMeeting() {
+        department.meeting();
+        System.out.println("営業として、上記の会議に出席します。部署：" + department.getName() + "名前" + name);
+    }
+}
